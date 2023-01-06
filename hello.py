00a06f35d7a609d7a59c8416f9525759f622c6bc
dal-fl <dal.flores.ar@gmail.com>
2023-01-06 05:43:31 -0800

    primera clase

---
+++ hello.py
@@ -0,0 +1,20 @@
+#Imprimir una cadena de texto
+print('hola, Solarsido')
+#Consultar tipo de dato
+print(type ('Hola, Solarsido'))
+
+#Esto es un comentario
+
+'''
+Esto es un
+Comentario de
+Varias lineas
+'''
+
+#Tipos de datos
+print(type('Soy un dato str')) #Tipo 'str'
+print(type(5)) #Tipo de dato 'int'
+print(type(1.5)) #Tipo de dato 'float'
+print(type(True)) #Tipo de dato 'bool'
+print(type(2+1j)) #Tipo de dato 'complex'
+
