9e3d1731df883b347eccb71da2f885eacdfc6adc
dal-fl <dal.flores.ar@gmail.com>
2023-01-07 07:02:01 -0800

    operadores

---
+++ 02_operators.py
@@ -0,0 +1,44 @@
+# Operadores aritmeticos #
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 // 3)
+print(2 ** 3) #Exponente
+print(2 ** 3 + 3 - 7 / 1 //4)
+
+print('Hola, ' + 'Python. ' 'Que tal?')
+#print('Hola' + 5) #TypeError
+print('Hola ' + str(5))
+print('Hola ' * 5)
+print('Hola ' * (2**3))
+
+my_float = 2.5 * 2
+print('Hola ' * int(my_float))
+
+# Operadores comparativos #
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(4 <= 4)
+print(4 == 4)
+print(3 != 4)
+
+print('Hola' > 'Python')
+print('Hola' < 'Python')
+print('Hola' >= 'Zola') #Orden alfabetico por ASCII
+print(len('aaa') <= len('abaa')) #Cuenta caracteres
+print('Hola' == 'Hola')
+print('Hola' != 'Python')
+
+# Operadores logicos #
+
+print(3 > 4 and 'Hola' > 'Python')
+print(3 > 4 or 'Hola' > 'Python')
+print(3 < 4 and 'Hola' < 'Python')
+print(3 < 4 or 'Hola' > 'Python')
+print(3 < 4 or 'Hola' > 'Python')
+print(3 > 4)
